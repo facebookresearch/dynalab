337dddcd223e39314810552d0d89f494c521b089
Zhiyi Ma <mzy0369@gmail.com>
2021-04-22 18:18:02 +0100

    [feat] Integrated test retry on top of torchserve built-in retry (#49)
    * [feat] Integrated test retry on top of torchserve built-in retry for model inference
    
    * [demo] Docker run cpu and memory limit specific to ml.m5.xlarge
    
    * [feat] Integrated test retry on top of torchserve built-in retry for model inference

--- dynalab/dockerfiles/dev-docker-entrypoint.sh
+++ dynalab/dockerfiles/dev-docker-entrypoint.sh
@@ -7,7 +7,7 @@ model_name=$1
 config_path="/home/model-server/code/.dynalab/${model_name}/setup_config.json"
 task=$(python -c "import json; config=json.load(open('$config_path')); print(config['task'])")
 
-echo Running integrated test for model $model_name on task $task
+echo "Running integrated test for model $model_name on task $task"
 
 set -m
 
@@ -20,20 +20,29 @@ while [[ $status != Healthy ]]; do
         echo "Serving model failed."
         exit 1
     fi
-    sleep 10
+    sleep 30
     status=$(curl -s http://localhost:8080/ping | python -c "import json, sys; obj=json.load(sys.stdin);print(obj['status'])") || exit 1
 done
+echo "Health ping passed. Start model inference..."
 
 # test the model endpoint, consider moving all these into a python script in the future?
 data=$(python -c "from dynalab.tasks.${task} import TaskIO; taskIO = TaskIO(); print(taskIO.get_input_json())")
 echo Input test data is $data
 response=$(curl http://127.0.0.1:8080/predictions/$model_name --header "Content-Type: application/json" --data "$data" --request POST --fail)
-if [ -z $response ]; then
+retries=0
+while [[ -z $response ]] && [[ $retries -le 3 ]]; do
+    echo "Model $model_name failed to respond. Retry in 30s [$retries / 3]"
+    retries=$(($retries+1))
+    sleep 30
+    response=$(curl http://127.0.0.1:8080/predictions/$model_name --header "Content-Type: application/json" --data "$data" --request POST --fail)
+done
+
+if [[ -z $response ]]; then
     ex=$(curl http://127.0.0.1:8080/predictions/$model_name --header "Content-Type: application/json" --data "$data" --request POST)
-    echo The exception message from $model_name is $ex
+    echo "The exception message from $model_name is $ex"
     exit 1
 else
-    echo Your model $model_name response is $response
+    echo "Your model $model_name response is $response"
     python -c "from dynalab.tasks.${task} import TaskIO; taskIO = TaskIO($data); taskIO.verify_response($response)" || exit 1
 fi
 
