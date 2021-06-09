0479a596229df05f4386b8fc9aed591dde317a24
Guillaume Wenzek <guw@fb.com>
2021-06-09 16:08:46 +0200

    add gpu support for local/docker test

--- dynalab_cli/test.py
+++ dynalab_cli/test.py
@@ -8,10 +8,13 @@ import os
 import shutil
 import subprocess
 import sys
+import logging
+from pathlib import Path
 
 from dynalab_cli import BaseCommand
 from dynalab_cli.utils import SetupConfigHandler
 
+logger = logging.getLogger(__name__)
 
 MAX_SIZE = 2 * 1024 * 1024 * 1024  # 2GB
 
@@ -32,6 +35,22 @@ class TestCommand(BaseCommand):
     def __init__(self, args):
         self.args = args
         self.config_handler = SetupConfigHandler(args.name)
+
+    def use_gpu(self, config) -> bool:
+        task = config["task"]
+        # TODO: should this be stored in the DB ? And exposed in the /tasks API ?
+        should_use_gpu = "flores" in task
+        gpu_available = sys.platform == "linux"
+
+        if should_use_gpu and not gpu_available:
+            logger.warning(
+                f"Task {task} should run on GPU, but this is not supported"
+                f" on platform '{sys.platform}'. Will runs the tests on CPU."
+            )
+            return False
+        if should_use_gpu:
+            logger.info(f"Will run tests on GPU for task {task}")
+        return should_use_gpu
 
     def run_command(self):
         # Validate config file: all keys exist, required values specified,
@@ -64,132 +83,155 @@ class TestCommand(BaseCommand):
 
         if self.args.local:
             self.run_local_test(config)
-
         else:
-            tmp_dir = os.path.join(self.config_handler.config_dir, "tmp")
-            os.makedirs(tmp_dir, exist_ok=True)
-            # tarball everything
-            print("Tarballing the project directory...")
-            exclude_list_file = os.path.join(tmp_dir, "exclude.txt")
-            self.config_handler.write_exclude_filelist(
-                exclude_list_file, self.args.name, exclude_model=True
-            )
-            process = subprocess.run(
-                [
-                    "tar",
-                    f"--exclude-from={exclude_list_file}",
-                    "-czf",
-                    os.path.join(tmp_dir, f"{self.args.name}.tar.gz"),
-                    ".",
-                ],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
-            )
-            if process.returncode != 0:
-                raise RuntimeError(
-                    f"Exception in tarballing the project directory {process.stderr}"
-                )
+            self.run_docker_test(config)
 
-            # torch model archive
-            print("Archive the model for torchserve...")
-            archive_command = [
-                "torch-model-archiver",
-                "--model-name",
-                self.args.name,
-                "--serialized-file",
-                config["checkpoint"],
-                "--handler",
-                config["handler"],
-                "--version",
-                "1.0",
-                "-f",
-                "--export-path",
-                tmp_dir,
-            ]
-            if config["model_files"]:
-                archive_command += ["--extra-files", ",".join(config["model_files"])]
-            process = subprocess.run(
-                archive_command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
-            )
-            if process.returncode != 0:
-                raise RuntimeError(f"Exception in torchserve archive {process.stderr}")
-
-            # pull docker
-            lib_dir = os.path.join(
-                os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-                "dynalab",
-                "dockerfiles",
-            )
-            docker_path = os.path.join(tmp_dir, "Dockerfile")
-            # TODO: pull the files from dynalab repo once public
-            shutil.copyfile(os.path.join(lib_dir, "Dockerfile.dev"), docker_path)
-            shutil.copyfile(
-                os.path.join(lib_dir, "dev-docker-entrypoint.sh"),
-                os.path.join(tmp_dir, "dev-docker-entrypoint.sh"),
-            )
-
-            # build docker
-            repository_name = self.args.name.lower()
-            print("Building docker image...")
-            docker_build_args = [
-                "--build-arg",
-                f"add_dir={tmp_dir}",
-                "--build-arg",
-                f"model_name={self.args.name}",
-                "--build-arg",
-                f"requirements={str(config['requirements'])}",
-                "--build-arg",
-                f"setup={str(config['setup'])}",
-            ]
-            docker_build_command = [
-                "docker",
-                "build",
-                "--network",
-                "host",
-                "-t",
-                repository_name,
-                "-f",
-                docker_path,
+    def run_docker_test(self, config):
+        tmp_dir = os.path.join(self.config_handler.config_dir, "tmp")
+        os.makedirs(tmp_dir, exist_ok=True)
+        # tarball everything
+        print("Tarballing the project directory...")
+        exclude_list_file = os.path.join(tmp_dir, "exclude.txt")
+        self.config_handler.write_exclude_filelist(
+            exclude_list_file, self.args.name, exclude_model=True
+        )
+        process = subprocess.run(
+            [
+                "tar",
+                f"--exclude-from={exclude_list_file}",
+                "-czf",
+                os.path.join(tmp_dir, f"{self.args.name}.tar.gz"),
                 ".",
-            ] + docker_build_args
-
-            subprocess.run(docker_build_command)
-            # NOTE: cpu and memory limit are specific to ml.m5.xlarge
-            process = subprocess.run(
-                f"docker run --network none --cpus 4 --memory 16G {repository_name}",
-                shell=True,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        if process.returncode != 0:
+            raise RuntimeError(
+                f"Exception in tarballing the project directory {process.stderr}"
             )
-            ts_log = os.path.join(tmp_dir, "ts_log.log")
-            with open(ts_log, "w") as f:
-                f.write(process.stderr)
-            if process.returncode != 0:
-                raise RuntimeError(
-                    f"Integrated test failed. Please refer to "
-                    f"{ts_log} for detailed torchserve log."
-                )
-            else:
-                print(
-                    f"Integrated test passed. "
-                    f"Torchserve log can be found at {ts_log}"
-                )
 
-            # clean up local tarball, .mar and intermediate docker layers
-            os.remove(os.path.join(tmp_dir, f"{self.args.name}.tar.gz"))
-            os.remove(os.path.join(tmp_dir, f"{self.args.name}.mar"))
+        # torch model archive
+        print("Archive the model for torchserve...")
+        archive_command = [
+            "torch-model-archiver",
+            "--model-name",
+            self.args.name,
+            "--serialized-file",
+            config["checkpoint"],
+            "--handler",
+            config["handler"],
+            "--version",
+            "1.0",
+            "-f",
+            "--export-path",
+            tmp_dir,
+        ]
+        if config["model_files"]:
+            archive_command += ["--extra-files", ",".join(config["model_files"])]
+        process = subprocess.run(
+            archive_command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        if process.returncode != 0:
+            raise RuntimeError(f"Exception in torchserve archive {process.stderr}")
+
+        # pull docker
+        lib_dir = os.path.join(
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+            "dynalab",
+            "dockerfiles",
+        )
+
+        use_gpu = self.use_gpu(config)
+        docker_file = Path(lib_dir) / "Dockerfile.dev"
+        if use_gpu:
+            docker_file = Path(lib_dir) / "Dockerfile.cuda"
+        docker_path = os.path.join(tmp_dir, "Dockerfile")
+        # TODO: pull the files from dynalab repo once public
+        shutil.copyfile(str(docker_file), docker_path)
+        shutil.copyfile(
+            os.path.join(lib_dir, "dev-docker-entrypoint.sh"),
+            os.path.join(tmp_dir, "dev-docker-entrypoint.sh"),
+        )
+
+        # build docker
+        repository_name = self.args.name.lower()
+        print("Building docker image...")
+        docker_build_args = [
+            "--build-arg",
+            f"add_dir={tmp_dir}",
+            "--build-arg",
+            f"model_name={self.args.name}",
+            "--build-arg",
+            f"requirements={str(config['requirements'])}",
+            "--build-arg",
+            f"setup={str(config['setup'])}",
+        ]
+        docker_build_command = [
+            "docker",
+            "build",
+            "--network=host",
+            "-t",
+            repository_name,
+            "-f",
+            docker_path,
+            ".",
+        ] + docker_build_args
+
+        subprocess.run(docker_build_command)
+        # NOTE: cpu and memory limit are specific to ml.m5.xlarge
+        # breakpoint()
+        docker_run = [
+            "docker",
+            "run",
+            "--network=none",
+            "--cpus=4",
+            "--memory=16G",
+            repository_name,
+        ]
+        if use_gpu:
+            docker_run.insert(-1, "--gpus=all")
+        process = subprocess.run(
+            docker_run,
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        image_id = subprocess.check_output(
+            ["docker", "ps", "-alq"],
+            encoding="utf-8",
+        ).strip()
+        ts_log = os.path.join(tmp_dir, "ts_log.err")
+        with open(ts_log, "w") as f:
+            f.write(process.stderr)
+        with open(os.path.join(tmp_dir, "ts_log.out"), "w") as f:
+            f.write(process.stdout)
+
+        if process.returncode != 0:
+            raise RuntimeError(
+                f"Integrated test failed. Please refer to "
+                f"{ts_log} for detailed torchserve log."
+            )
+        else:
             print(
-                "We suggest removing unused docker data by `docker system prune`, "
-                "including unused containers, networks and images. "
-                "To do so, choose 'y' for the following prompt. "
-                "More info available at "
-                "https://docs.docker.com/engine/reference/commandline/system_prune/"
+                f"Integrated test passed. " f"Torchserve log can be found at {ts_log}"
             )
-            subprocess.run(f"docker system prune", shell=True)
+
+        # clean up local tarball, .mar and intermediate docker layers
+        os.remove(os.path.join(tmp_dir, f"{self.args.name}.tar.gz"))
+        os.remove(os.path.join(tmp_dir, f"{self.args.name}.mar"))
+        print(
+            "We suggest removing unused docker data by `docker system prune`, "
+            "including unused containers, networks and images. "
+            "To do so, choose 'y' for the following prompt. "
+            "More info available at "
+            "https://docs.docker.com/engine/reference/commandline/system_prune/"
+        )
+        subprocess.run(f"docker system prune", shell=True)
 
     def run_local_test(self, config):
         # load handler
@@ -204,7 +246,9 @@ class TestCommand(BaseCommand):
         taskIO = importlib.import_module(f"dynalab.tasks.{config['task']}").TaskIO()
         mock_data = importlib.import_module(f"dynalab.tasks.{config['task']}").data
         try:
-            taskIO.mock_handle_individually(self.args.name, mock_data, handler.handle)
+            taskIO.mock_handle_individually(
+                self.args.name, mock_data, self.use_gpu(config), handler.handle
+            )
         except Exception as e:
             raise RuntimeError(f"Local test failed because of: {e}")
         else:
