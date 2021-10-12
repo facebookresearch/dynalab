523cc83d488fc0099c1587633a69685a1c6d073d
TristanThrush <tthrush@fb.com>
2021-10-12 12:30:59 -0700

    dynalab side of big data pr (#115)
    * dynalab side of big data pr
    
    * added base64 string directly

--- dynalab/tasks/annotation_mock_data.py
+++ dynalab/tasks/annotation_mock_data.py
@@ -12,8 +12,19 @@ def get_source_data(annotation, name_to_annotation_dict):
     return source_data
 
 
-def generate_image_url_mock_data(annotation=None, name_to_annotation_dict=None):
-    return ["https://dynabench.org/logo_w.png"]
+def generate_image_mock_data(annotation=None, name_to_annotation_dict=None):
+    base64_image_string = (
+        'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABx0lEQVQ4T2P8'
+        + '////fwYKACPVDKhq\nWcHw9etPsFvevPvMICLEy/Dt+y8GBob/DFyc7HAxkDwXJxtDe10kWC3cBQ'
+        + 'VVCxkmtMUz/P79h2Hq\n3N0MBRmeDFt3nWf49/8/g6+7EcPUuTsZspPdwZryKxcyTGyPRz'
+        + 'UgJmMKg6mhMgMHOyvDnkNXGfS0\nZBmmX3jC8J+RkeFSfzzDqo3H8RsAcwHI2IkzdzCkxDoy'
+        + 'WKRNYwAFcaKVKsPJs3cYbC01Gf7+/cdw\n6twdhmWzcrF7AWZAfroHWBMokixMVIn0go'
+        + 'EyAxMzE8OBo9cYbC01GB49fsMQ6G0KNiC9aDZDcbYP\ng7aGDPYwQPcCyAVHT95kWLv5FMO7D'
+        + '18Y9hleZuA6yMmQYe3CcOosHi8sXLKPYdPSXQwT55YxPH3+\nDuwFYSFehqCGPoYIc0uG6uxA'
+        + '3LFgYqDMcGbJRoZslmcMk9XcGJQVxRi8XQ2JCwOYFw4fv8HQN20r\nw+IZ2QxHT95i2LzzLIO'
+        + 'KogTD8dO3GBxttBl+/PxNXixMmbOTIT3emYGVlQW7F0BJ+cvXH5Ck/PYL\ng4gwD8P3778YQD'
+        + 'kNlHTfvvvCICzEA5bn5mTHTMrkZkiKcyMApgcb4F7rmG8AAAAASUVORK5CYII=\n')
+    return [base64_image_string]
 
 
 def generate_string_mock_data(annotation=None, name_to_annotation_dict=None):
@@ -68,7 +79,7 @@ def generate_target_label_mock_data(annotation, name_to_annotation_dict=None):
 
 
 annotation_mock_data_generators = {
-    AnnotationTypeEnum.image_url.name: generate_image_url_mock_data,
+    AnnotationTypeEnum.image.name: generate_image_mock_data,
     AnnotationTypeEnum.string.name: generate_string_mock_data,
     AnnotationTypeEnum.context_string_selection.name: generate_context_string_selection_mock_data,
     AnnotationTypeEnum.conf.name: generate_conf_mock_data,
